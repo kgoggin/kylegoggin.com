96c86d23e05540cb8813f4f27027025765e8c367
Kyle Goggin <kgoggin@mywell.org>
2018-04-10 10:01:46 -0400

    Reason/GraphQL post + Prism code highlighting

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Masthead from "../components/MastHead";
+require("prismjs/themes/prism-tomorrow.css");
 
 class Template extends React.Component {
   render() {
-    const {children} = this.props;
+    const { children } = this.props;
     return (
       <div>
         <Masthead />
-        <div css={{margin: "0 2rem"}}>{children()}</div>
+        <div css={{ margin: "0 2rem" }}>{children()}</div>
       </div>
     );
   }
