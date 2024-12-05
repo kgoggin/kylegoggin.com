1ab5e19c17bf6c67027deeba05e3361b4f3c7106
Kyle Goggin <kyle@kylegoggin.com>
2024-12-05 07:12:10 -0500

    add more TS support

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
