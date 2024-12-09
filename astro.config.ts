6c1bc43a853d573f0db497c812bcaf3141f9bf70
Kyle Goggin <kyle@kylegoggin.com>
2024-12-09 06:04:26 -0500

    add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -3,10 +3,15 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.kylegoggin.com",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
